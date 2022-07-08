757fc9a8801b34dfe09609fcc03527e059721ce8
Yuvento <yuventocungiono@gmail.com>
2022-07-08 20:19:11 +0700

    perubahan

--- index.js
+++ index.js
@@ -1,86 +1,98 @@
-// angka genap
+// // angka genap
 
-function solution(n){
-    //tulis jawabanmu disini
-    if(n>2 && n%2===0) {return "YES"}
-    else {return "NO"}
-}
+// function solution(n){
+//     //tulis jawabanmu disini
+//     if(n>2 && n%2===0) {return "YES"}
+//     else {return "NO"}
+// }
 
-// nilai ujian
-function solution(n){
-    // tulis jawabanmu disini
+// // nilai ujian
+// function solution(n){
+//     // tulis jawabanmu disini
     
-    if(n>=91) return "A";
-    else if (n>=81 && n<91) return "B";
-     else if (n>=71 && n<81) return "C";
-      else if (n>=61 && n<71) return "D";
-      else return "E";
-}
+//     if(n>=91) return "A";
+//     else if (n>=81 && n<91) return "B";
+//      else if (n>=71 && n<81) return "C";
+//       else if (n>=61 && n<71) return "D";
+//       else return "E";
+// }
 
 
-// fizzbuzz
-function solution(n){
-    // tulis jawabanmu disini
+// // fizzbuzz
+// function solution(n){
+//     // tulis jawabanmu disini
 
-    if(n%5===0&&n%3===0) return "FizzBuzz";
-    else if(n%5===0) return "Fizz";
-        else if(n%3===0) return "Buzz";
-            else return -1;
+//     if(n%5===0&&n%3===0) return "FizzBuzz";
+//     else if(n%5===0) return "Fizz";
+//         else if(n%3===0) return "Buzz";
+//             else return -1;
             
-}
+// }
 
-//  serifikasi
+// //  serifikasi
 
-function solution(n, b, p){
-    let nilai = n - b;
-    let total = nilai*-1;
-    let total2 = b*3; 
-    if((total2+total)>=p) return "YES";
-    else return "NO";
+// function solution(n, b, p){
+//     let nilai = n - b;
+//     let total = nilai*-1;
+//     let total2 = b*3; 
+//     if((total2+total)>=p) return "YES";
+//     else return "NO";
 
 
     
-}
+// }
 
-// narik uang
-function solution(x, y){
-    // tulis jawabanmu disini
-  let z= (y-x)-0.5;
-  if(x<y&&x%5===0)return z;
-  else return y;
+// // narik uang
+// function solution(x, y){
+//     // tulis jawabanmu disini
+//   let z= (y-x)-0.5;
+//   if(x<y&&x%5===0)return z;
+//   else return y;
 
    
-}
+// }
 
-// menghitung total
-function solution(num){
-    // tulis jawaban disini
-    let sum=0;
-    for(let i=0;i<=num;i++)
-         sum+=i;
-        return sum;
-}
+// // menghitung total
+// function solution(num){
+//     // tulis jawaban disini
+//     let sum=0;
+//     for(let i=0;i<=num;i++)
+//          sum+=i;
+//         return sum;
+// }
 
-// jumlah digit angka
+// // jumlah digit angka
 
-function solution(num){
-    // tulis jawaban disini
-      return String(num)
-    .split('')
-    .map(Number)
-    .reduce((a,b)=>{return a+b},0);    
-}
+// function solution(num){
+//     // tulis jawaban disini
+//       return String(num)
+//     .split('')
+//     .map(Number)
+//     .reduce((a,b)=>{return a+b},0);    
+// }
 
-// item terbesar
+// // item terbesar
 
-function solution(arr){
-    let max =  0;
-    for(let i=0;i<arr.length;i++)
-       if(arr[i]>max)
+// function solution(arr){
+//     let max =  0;
+//     for(let i=0;i<arr.length;i++)
+//        if(arr[i]>max)
 
-       max = arr[i];
+//        max = arr[i];
 
-       return max;
+//        return max;
 
   
+// }
+
+
+
+function solution (str1,str2){
+    let bolak =  str2.split("").reverse().join("")
+
+    if(str1===bolak) return "YES"
+    else return "NO"
 }
+
+console.log(solution("kode","edo3"))
+
